b8662374798cc0bc912ee425fa52ce25a99792df
FiFlyC <combolev@gmail.com>
2022-02-10 19:41:51 +0300

    Add solution for the medium part

--- hw2/run.sh
+++ hw2/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 python3 src/tabular.py in.csv
+python3 src/graphics.py in.csv
+
+rm artifacts/*.{toc,out,aux,log}
+pdflatex -output-directory=artifacts/ artifacts/out_medium.tex
+rm artifacts/*.{toc,out,aux,log}
+rm artifacts/out_medium.tex
