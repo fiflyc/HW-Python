9bffde638f53caa00584a087a4b19f22b76c3925
FiFlyC <combolev@gmail.com>
2022-02-26 18:28:45 +0300

    Add solution for the medium part

--- hw4/run.py
+++ hw4/run.py
@@ -1,12 +1,15 @@
+import sys
+from os import cpu_count
 from src.thread_proc_test import (
 	test_seq,
 	test_threads,
 	test_proc
 )
+from src.integrate import test_int_threads, test_int_procs
 
 
 def easy():
-	n = 600000
+	n = 200000
 	c = 10
 
 	with open('artifacts/time_easy.txt', 'w') as out:
@@ -15,5 +18,23 @@ def easy():
 		out.write(f'Multiprocessing: {test_proc(n, c):.4f}s\n')
 
 
+def medium():
+	with open('artifacts/time_medium.txt', 'w') as out:
+		for n_jobs in range(1, cpu_count() * 2):
+			time_t = test_int_threads(n_jobs, 'artifacts/logs_threads_medium.txt')
+			time_p = test_int_procs(n_jobs, 'artifacts/logs_procs_medium.txt')
+			out.write(f'ThreadPoolExecutor  (jobs={n_jobs}):\t{time_t:.6f}s\n')
+			out.write(f'ProcessPoolExecutor (jobs={n_jobs}):\t{time_p:.6f}s\n')
+
+
 if __name__ == '__main__':
-	easy()
+	if len(sys.argv) < 2:
+		print("Usage: python3 run.py [PART]\nwhere PART = easy | medium | hard")
+	elif sys.argv[1] == 'easy':
+		easy()
+	elif sys.argv[1] == 'medium':
+		medium()
+	elif sys.argv[1] == 'hard':
+		pass
+	else:
+		print("Usage: python3 run.py [PART]\nwhere PART = easy | medium | hard")
